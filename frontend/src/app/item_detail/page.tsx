6d90193d2ad76f84108d3fa0ec75b164d6ea3475
Nguyen Hien <hiennguyenhien1105@gmail.com>
2024-03-27 01:38:08 +0700

    [FE] credit card + place bid

--- frontend/src/app/item_detail/page.tsx
+++ frontend/src/app/item_detail/page.tsx
@@ -8,8 +8,13 @@ import VerticalSlide from "@/components/item/VerticalSlide";
 import Item from "@/models/item";
 import { get_detail_item } from "@/services/item/item";
 
+import { Modal } from 'react-bootstrap';
 
 export default function Item(props: Item) {
+  const [showModal, setShowModal] = useState(false);
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
+
   const [itemData, setItemData] = useState({
       "id": 2,
       "title": "name",
@@ -73,9 +78,9 @@ export default function Item(props: Item) {
             <div className={`${style['main-lot-title']} ${style['d-none']} ${style['d-md-block']}`}>
               <div>
                 <div className={style['artist-title']}>
-                  <a href="/" target="_blank" rel="noreferrer" className={style['black-link']}><div>{itemData.artist}</div></a>
+                  <a href="/" target="_blank" rel="noreferrer" className={style['black-link']}><div>{itemData?.artist}</div></a>
                 </div>
-                <h1 className={`${style['title']} ${style['mb-4']}`}>Lot {itemData.id}:<span className="font-italic">&nbsp;{itemData.title}</span></h1>
+                <h1 className={`${style['title']} ${style['mb-4']}`}>Lot {itemData?.id}:<span className="font-italic">&nbsp;{itemData?.title}</span></h1>
               </div>
             </div>
           </Container>
@@ -86,7 +91,7 @@ export default function Item(props: Item) {
           <Container>
             <div className={style['contend-aside-holder']}>
               <div className={style.contend}>
-                <VerticalSlide  images={itemData.images.map(image => image.path)}></VerticalSlide>
+                {/* <VerticalSlide  images={itemData.images.map(image => image.path)}></VerticalSlide> */}
                 <div className={style['sharing-btn']}>
                   <i className="fa fa-envelope"></i>
                   <i className="fa fa-share"></i>
@@ -105,20 +110,20 @@ export default function Item(props: Item) {
                             <div>
                               <h4>Description</h4>
                               <div>
-                                <b>{itemData.artist}</b> <br />
+                                <b>{itemData?.artist}</b> <br />
 
-                                <p>{itemData.description}</p>
+                                <p>{itemData?.description}</p>
                               </div>
                             </div>
                             <div>
                               <h4>Dimensions</h4>
-                              <div>{itemData.dimensions}</div>
+                              <div>{itemData?.dimensions}</div>
                             </div>
                             <div>
                               <h4>Artist or Maker</h4>
                               <div className="artist-info">
                                 <a href="/artist/nieto-john-w-d664fsop1m" target="_blank" rel="noreferrer">
-                                  <div>{itemData.artist}</div>
+                                  <div>{itemData?.artist}</div>
                                 </a>
                               </div>
                             </div>
@@ -129,7 +134,7 @@ export default function Item(props: Item) {
                             <div>
                               <h4>Condition Report</h4>
                               <div>
-                                <i>{itemData.condition_report}</i>
+                                <i>{itemData?.condition_report}</i>
                               </div>
                             </div>
                             <div>
@@ -174,34 +179,143 @@ export default function Item(props: Item) {
                   </div>
                 </div>
               </div>
-              {itemData.status === 'sold' ? (
-                <p className={style.est}>Est: ${itemData.estimate_min} USD - ${itemData.estimate_max} USD</p>
+              {itemData?.status === 'sold' ? (
+                <p className={style.est}>Est: ${itemData?.estimate_min} USD - ${itemData?.estimate_max} USD</p>
               ) : (<div className={style.aside}>
                 <div className={style['bid-room-inner']}>
                   <div className={style['bid-status']}>
-                    <p className={style.est}>Est: ${itemData.estimate_min} USD - ${itemData.estimate_max} USD</p>
-                    <p className={style.usd}><b>${itemData.max_bid} USD</b><span className={style['bid-count']}>     {itemData.count_bid} bids</span></p>
+                    <p className={style.est}>Est: ${itemData?.estimate_min} USD - ${itemData?.estimate_max} USD</p>
+                    <p className={style.usd}><b>${itemData?.max_bid} USD</b><span className={style['bid-count']}>     {itemData?.count_bid} bids</span></p>
                   </div>
-                  <Form action="" className={style['form-group-bid']}>
+                  <form action="" className={style['form-group-bid']}>
                     <label htmlFor=""><span>Set Max Bid:</span></label>
                     <div className={style['select-dropdown']}>
                       <select className={`${style['form-control']} ${style['select-single']}`} >
                         {/* Kiểm tra xem max_bid có tồn tại và không phải là undefined không trước khi tạo các option */}
-                        {itemData.max_bid !== undefined && [...Array(10)].map((_, index) => (
-                          <option key={index}>${itemData.max_bid! + index * 5} USD</option>
+                        {itemData?.max_bid !== undefined && [...Array(10)].map((_, index) => (
+                          <option key={index}>${itemData?.max_bid! + index * 5} USD</option>
                         ))}
                       </select>
                     </div>
-                    <button type="button" className={`btn btn-primary btn-lg btn-block ${style['btn-place-bid']} ${style['button-style']}`}>Place Bid</button>
+                        <button type="button" className={`btn btn-primary btn-lg btn-block ${style['btn-place-bid']} ${style['button-style']}`} onClick={handleShowModal}>Place Bid</button>
                     <div className={style.secure}>
                       <span className={style['secure-bidding']}><i className="fa fa-lock"></i>Secure Bidding</span>
                     </div>
 
-                  </Form>
+                  </form>
                 </div>
               </div>)}
 
             </div>
+
+            <Modal show={showModal} onHide={handleCloseModal} size="lg">
+                <Modal.Header closeButton>
+                </Modal.Header>
+                <Modal.Body>
+                  <h2 className="text-center">Review Your Bid</h2>
+                    <p>Select the highest amount you are willing to bid. If you are outbid, we will increase your bid by one increment, up to your max.</p>
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <td style={{ border: 'none' }}>Estimate</td>
+                                <td style={{ border: 'none' }}>$0 USD - $0 USD</td>
+                            </tr>
+                            <tr>
+                                <td style={{ border: 'none' }}>Current bid</td>
+                                <td style={{ border: 'none' }}>$1 USD</td>
+                            </tr>
+                            <tr>
+                                <td style={{ border: 'none' }}>Your max bid</td>
+                                <td style={{ border: 'none' }}>
+                                    <select className="form-select">
+                                        <option>$2 USD</option>
+                                        <option>$0.44 USD</option>
+                                        <option>$2.44 USD (+shipping, taxes & fees)</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style={{ border: 'none' }}>Buyer's Premium</td>
+                                <td style={{ border: 'none' }}>$0.44 USD</td>
+                            </tr>
+                            <tr>
+                                <td style={{ border: 'none' }}>TOTAL</td>
+                                <td style={{ border: 'none' }}>$2.44 USD (+ shipping, taxes & fees)</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <hr />
+                    <h5>Shipping Address</h5>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" placeholder="First Name" />
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" placeholder="Last Name" />
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" placeholder="Address Line 1" />
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col">
+                            <input type="text" className="form-control" placeholder="City" />
+                        </div>
+                        <div className="col">
+                            <select className="form-select">
+                                <option>Country</option>
+                                {/* Thêm các option cho select country */}
+                            </select>
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col">
+                            <input type="text" className="form-control" placeholder="Zip/Postal Code" />
+                        </div>
+                        <div className="col">
+                            <input type="text" className="form-control" placeholder="Province" />
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" placeholder="Phone Number" />
+                    </div>
+                    <hr />
+                    <h5>Credit Card</h5>
+                    <p>You won’t be charged unless you win. If you win, the auction house may auto-charge this card 2 days after the invoice is sent.</p>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" placeholder="Card Number" />
+                    </div>
+                    <p><i className="fa fa-lock" aria-hidden="true"></i><span className='mx-1'>Security server <a rel="noopener noreferrer" target="_blank" style={{color: "#004bd6"}}>Certified by VikingCloud</a></span></p>
+                    <div className="mb-3 d-flex">
+                        <input type="text" className="form-control me-2" placeholder="MM/YY" />
+                        <input type="text" className="form-control" placeholder="CVN" />
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" placeholder="Name on Card" />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="billingAddress" />
+                        <label className="form-check-label" htmlFor="billingAddress">Billing address is same as shipping address</label>
+                    </div>
+                    <hr />
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="textMe" />
+                        <label className="form-check-label" htmlFor="textMe">Text me if I'm outbid and before the auction starts for my lots</label>
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col">
+                            <select className="form-select">
+                                <option>Country</option>
+                                {/* Thêm các option cho select country */}
+                            </select>
+                        </div>
+                        <div className="col">
+                            <input type="text" className="form-control" placeholder="Phone Number" />
+                        </div>
+                    </div>
+                    <button type="button" className="btn btn-dark w-100">Place Bid</button>
+                    <p className="text-center mt-2">By clicking "Place Bid", you agree to submit a bid for this item for the amount described above, not including shipping, taxes, and fees. All accepted bids are binding. Your address and payment information on file will be encrypted and securely submitted using SSL technology in order to complete your registration for this sale. After the sale, the seller may auto-charge the payment method used during registration.apply.</p>
+
+                </Modal.Body>
+            </Modal>
           </Container >
         </div >
 
--- frontend/src/app/my-account/payment-options/page.tsx
+++ frontend/src/app/my-account/payment-options/page.tsx
@@ -1,42 +1,113 @@
 'use client'
+import { useState } from 'react';
+import { Modal } from 'react-bootstrap';
 import style from '../style.module.css';
-import React, { useState } from 'react';
-import SideBarMyAccount from "@/components/my-account/sideBar";
 
 
 export default function PaymentOptions() {
-
+    const [showModal, setShowModal] = useState(false);
+    const handleCloseModal = () => setShowModal(false);
+    const handleShowModal = () => setShowModal(true);
 
     return (
-        <div className='row mx-0'>
-            {/* <div className="col-2">
-                <SideBarMyAccount />
-            </div> */}
-            {/* <div className="col-10 px-5"> */}
-                <div className={style.div_title}>
-                    Payment Options
-                </div>
-                <div className={style.div_section}>
-                    <div className={style.div_header}>
-                        Credit Cards
-                    </div>
-                    <p>
-                    Add a credit card to make bidding fast and easy.
-                    </p>
-                    <button type="button" className="btn btn-dark px-5">Add A New Card</button>
-                </div>
+        // <div className='row mx-0'>
+        //     {/* <div className="col-2">
+        //         <SideBarMyAccount />
+        //     </div> */}
+        //     {/* <div className="col-10 px-5"> */}
+        //         <div className={style.div_title}>
+        //             Payment Options
+        //         </div>
+        //         <div className={style.div_section}>
+        //             <div className={style.div_header}>
+        //                 Credit Cards
+        //             </div>
+        //             <p>
+        //             Add a credit card to make bidding fast and easy.
+        //             </p>
+        //             <button type="button" className="btn btn-dark px-5">Add A New Card</button>
+        //         </div>
 
 
 
-                <div className={style.div_section}>
-                    <div className={style.div_header}>
-                        Bank Accounts
-                    </div>
-                    <button type="button" className="btn btn-dark px-5">Add A Bank Account</button>
+        //         <div className={style.div_section}>
+        //             <div className={style.div_header}>
+        //                 Bank Accounts
+        //             </div>
+        //             <button type="button" className="btn btn-dark px-5">Add A Bank Account</button>
                    
-                </div>
+        //         </div>
 
-            {/* </div> */}
-        </div >
+        //     {/* </div> */}
+        // </div >
+
+        <div>
+            <div className="div_title">
+                Payment Options
+            </div>
+            <div className="div_section">
+                <div className="div_header">
+                    Credit Cards
+                </div>
+                <p>
+                    Add a credit card to make bidding fast and easy.
+                </p>
+                <button type="button" className="btn btn-dark px-5" onClick={handleShowModal}>Add A New Card</button>
+            </div>
+            <div className={style.div_section}>
+                <div className={style.div_header}>
+                    Bank Accounts
+                </div>
+                <button type="button" className="btn btn-dark px-5">Add A Bank Account</button>
+            </div>
+
+            <Modal show={showModal} onHide={handleCloseModal} size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>Add A New Card</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" id="cardNumber" placeholder="Card Number" />
+                    </div>
+                    <p><i className="fa fa-lock" aria-hidden="true"></i><span className='mx-1'>Security server <a rel="noopener noreferrer" target="_blank" style={{color: "#004bd6"}}>Certified by VikingCloud</a></span></p>
+                    <div className="mb-3 d-flex">
+                        <input type="text" className="form-control me-2" placeholder="MM/YY" />
+                        <input type="text" className="form-control" placeholder="CVN" />
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" id="nameOnCard" placeholder="Name on Card" />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="billingAddress" />
+                        <label className="form-check-label" htmlFor="billingAddress">Billing address is same as shipping address</label>
+                    </div>
+                    <div className="mb-3">
+                        <select className="form-select" id="country">
+                            <option>Select Country</option>
+                            {/* Thêm các option cho select country */}
+                        </select>
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" id="address" placeholder="Enter Address" />
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col">
+                            <input type="text" className="form-control" id="city" placeholder="Enter City" />
+                        </div>
+                        <div className="col">
+                            <input type="text" className="form-control" id="state" placeholder="Enter State" />
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" id="postalCode" placeholder="Enter Postal Code" />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer className="justify-content-start">
+                    <button type="button" className="btn btn-dark">Save Card</button>
+                    <p className="mx-3" onClick={handleCloseModal}>Cancel</p>
+                </Modal.Footer>
+
+            </Modal>
+        </div>
     );
 }
