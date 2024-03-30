9d03572f90bf39027eeeccce127376327e811f29
NguyenTranDat <21020011@vnu.edu.vn>
2024-03-31 03:57:47 +0700

    [FE + API] create auction

---
+++ frontend/src/services/auction/create.tsx
@@ -0,0 +1,19 @@
+"use client"
+import axios from 'axios';
+
+import { HOST } from '@/services/host';
+import Auction from '@/models/auciton';
+
+const auction_create_service = async (auctionData: Auction) => {
+    try {
+        let url = `${HOST}/auction/create`;
+        let response = await axios.post(url, auctionData);
+        console.log(response.data)
+        return;
+    } catch (error: any) {
+        console.error('Error fetching data:', error);
+        return error.response.data;
+    }
+};
+
+export default auction_create_service;
