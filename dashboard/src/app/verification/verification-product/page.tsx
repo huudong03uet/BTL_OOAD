3e0738e87b0b50a24a29a6ee377f9726d4d46e62
Nguyễn Ngọc Linh <100554756+nngoclinh1203@users.noreply.github.com>
2024-04-06 16:20:38 +0700

    [Dash] verification product auction

--- dashboard/src/app/verification/verification-product/page.tsx
+++ dashboard/src/app/verification/verification-product/page.tsx
@@ -3,6 +3,8 @@ import FormElements from "@/components/FormElements";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import TableProduct from "@/components/Verification/TableProduct";
+import { FormRegisterProduct } from "@/types/form_register_product";
 
 export const metadata: Metadata = {
   title: "Next.js Form Elements | TailAdmin - Next.js Dashboard Template",
@@ -10,10 +12,49 @@ export const metadata: Metadata = {
     "This is Next.js Form Elements page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
 };
 
+interface TableUserProps {
+  packageData: FormRegisterProduct[];
+}
+
 const VerificationProductPage = () => {
+
+  const packageDatafake: FormRegisterProduct[] = [
+    {
+      product_id: 78431,
+      title: "Free package",
+      time_create: `Jan 13,2023`,
+      status: 'pending',
+      seller: {name: "Linh", user_id:32834},
+    },
+    {
+      product_id: 7374,
+      title: "Free package",
+      time_create: `Jan 13,2023`,
+      status: 'pending',
+      seller: {name: "Linh", user_id:32834},
+
+    },
+    {
+      product_id: 345645,
+      title: "Free package",
+      time_create: `Jan 13,2023`,
+      status: 'pending',
+      seller: {name: "Linh", user_id:32834},
+
+    },
+    {
+      product_id: 234325,
+      title: "Free package",
+      time_create: `Jan 13,2023`,
+      status: 'pending',
+      seller: {name: "Linh", user_id:32834},
+
+    },
+  ];
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Verification Product"></Breadcrumb>
+      <TableProduct packageData={packageDatafake}></TableProduct>
     </DefaultLayout>
   );
 };
---
+++ dashboard/src/components/Modal/ModalProduct.tsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import { Modal, ModalContent, ModalFooter, ModalHeader, Button, ModalBody } from '@nextui-org/react';
+import { useSWRConfig } from "swr"
+import { mutate } from "swr"
+import { FormRegisterProduct } from '@/types/form_register_product';
+
+interface IProps {
+  showModalCreate: boolean;
+  setShowModalCreate: (value: boolean) => void;
+  productInformation: FormRegisterProduct;
+}
+
+function CreateModal(props: IProps) {
+  const { showModalCreate, setShowModalCreate, productInformation } = props;
+
+  const handleCloseModal = () => setShowModalCreate(false);
+  const handleShowModal = () => setShowModalCreate(true);
+
+  let { product_id, seller, title, images, estimate_min, estimate_max, description, dimensions, artist, category, condition_report, provenance, time_create, status } = productInformation;
+
+  const handleSubmit = () => {
+    status = "complete";
+
+    //call api sửa đổi trạng thái của product thành seller, be trả về với status là complete
+    fetch("http://localhost:8000/blogs",
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({ status })
+      })
+      .then(function (res) {
+        handleCloseModal();
+        mutate("http://localhost:8000/blogs")
+      })
+      .catch(function (res) { console.log(res) })
+
+  };
+
+  return (
+    <Modal
+      isOpen={showModalCreate}
+      onClose={handleCloseModal}
+      size='lg'
+      style={{ top: '250px', left: '30%', transform: 'translate(-50%, -50%)' }} // Đặt vị trí modal
+    >
+      <ModalContent>
+        <>
+          <ModalHeader className="flex flex-col gap-1">Verification Product</ModalHeader>
+          <ModalBody style={{ maxHeight: '300px', overflowY: 'auto' }}>
+            <div className="mb-0 ml-3">
+              <p><strong>Product Title:</strong> {title}</p>
+            </div>
+            <div className="mb-0 flex-row" style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className="ml-3"><strong>Seller:</strong> {seller && seller.name}</p>
+              <p className="mr-20"><strong>Artist:</strong> {artist}</p>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Description:</strong> {description}</p>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Dimensions:</strong> {dimensions}</p>
+            </div>
+            <div className="mb-0 flex-row" style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className='ml-3'><strong>Estimate: </strong> {estimate_min} - {estimate_max}</p>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Category:</strong></p>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                {category && category.map((obj, index) => (
+                  <div key={index}>
+                    <p>{obj.name}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Condition:</strong> {condition_report}</p>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Provenance:</strong> {provenance}</p>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Image:</strong></p>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                {images && images.map((image, index) => (
+                  <div key={index}>
+                    <img src={image.url} alt="" style={{ width: '30px', height: 'auto' }} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </ModalBody>
+          <ModalFooter style={{ justifyContent: 'space-between' }}>
+            <Button color="danger" onPress={handleCloseModal}>
+              Reject
+            </Button>
+            <Button color="primary" onPress={handleCloseModal}>
+              Accept
+            </Button>
+          </ModalFooter>
+        </>
+      </ModalContent>
+    </Modal>
+
+  );
+}
+
+export default CreateModal;
