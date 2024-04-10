c75585fc962b70c3510115d1afb86823ef6eddfe
NguyenTranDat <21020011@vnu.edu.vn>
2024-04-10 11:59:36 +0700

    [API] update product

--- frontend/src/app/seller/(product)/edit-product/page.tsx
+++ frontend/src/app/seller/(product)/edit-product/page.tsx
@@ -2,64 +2,50 @@
 import { Form, } from "react-bootstrap";
 import style from '../../../my-account/style.module.css'
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
-import UserDataService from "@/services/model/user";
-import { seller_add_product } from "@/services/product/seller";
+import ProductDetail from "@/models/product_detail";
+import { user_get_detail_product } from "@/services/product/user";
+import { seller_update_product } from "@/services/product/seller";
 
 
 export default function EditProduct() {
-    const [producttitle, setProducttitle] = useState<string>('');
-    const [productDescription, setProductDescription] = useState<string>('');
-    const [productArtist, setProductArtist] = useState<string>('');
     const [productCategory, setProductCategory] = useState<string>('');
-    const [productImages, setProductImages] = useState<File[]>([]);
-    const [user_id, setUserID] = useState<string | null>(UserDataService.getUserData()?.user_id?.toString() || null);
+    const [product, setProduct] = useState({} as ProductDetail)
 
-
-    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files && e.target.files.length > 0) {
-            const files: FileList = e.target.files;
-            const newImages: File[] = [];
-            for (let i = 0; i < files.length; i++) {
-                newImages.push(files[i]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await user_get_detail_product(72);
+                setProduct(data);
+                if (data.categories != null) {
+                    setProductCategory(data.categories[0].title);
+                }
+            } catch (error) {
+                console.error('Error fetching upcoming online auctions:', error);
             }
-            setProductImages([...productImages, ...newImages]);
-        }
-    };
+        };
 
-    const handleRemoveImage = (index: number) => {
-        const updatedImages = [...productImages];
-        updatedImages.splice(index, 1);
-        setProductImages(updatedImages);
+        fetchData()
+    }, [])
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setProduct({ ...product, [name]: value });
     };
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        let formData = new FormData();
-        formData.append("title", producttitle);
-        formData.append("description", productDescription);
-        formData.append("category_name", productCategory);
-        formData.append("artist", productArtist);
-
-        if (productImages.length > 0 && user_id !== null) {
-            formData.append("user_id", user_id);
-            productImages.forEach((image) => {
-                formData.append('images', image);
-            });
-
-            await seller_add_product(formData);
-        } else {
-            console.error('Hình ảnh không được để trống');
+        try {
+            await seller_update_product(product, productCategory);
+        } catch (error) {
+            console.error('Failed to update product:', error);
         }
     };
 
 
 
     return (
-        <div className='row mx-5'>
-
-            <div className={style.div_title}>
-                Edit product
-            </div>
+        <div className="row mx-5">
+            <div className={style.div_title}>Edit product</div>
             <form onSubmit={handleSubmit}>
                 <div className={style.div_section}>
                     <div className="row">
@@ -67,30 +53,32 @@ export default function EditProduct() {
                             <Form.Label>Product name</Form.Label>
                             <Form.Control
                                 type="text"
+                                name="title"
                                 placeholder="Product name"
                                 className={style.custom_form_control}
-                                value={producttitle}
-                                onChange={(e) => setProducttitle(e.target.value)}
+                                value={product.title}
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className="col-6">
                             <Form.Label>Artist</Form.Label>
                             <Form.Control
                                 type="text"
+                                name="artist"
                                 placeholder="Artist"
                                 className={style.custom_form_control}
-                                value={productArtist}
-                                onChange={(e) => setProductArtist(e.target.value)}
+                                value={product.artist}
+                                onChange={handleInputChange}
                             />
                         </div>
                     </div>
 
                     <div className="row">
-
                         <div className="col-12">
                             <Form.Label>Category</Form.Label>
                             <Form.Control
                                 type="text"
+                                name="category_name"
                                 placeholder="Category"
                                 className={style.custom_form_control}
                                 value={productCategory}
@@ -104,30 +92,22 @@ export default function EditProduct() {
                             <Form.Label>Min estimate (VNĐ)</Form.Label>
                             <Form.Control
                                 type="text"
+                                name="min_estimate"
                                 placeholder="Min estimate"
                                 className={style.custom_form_control}
-                                value={producttitle}
-                                // onChange={(e) => setProducttitle(e.target.value)}
+                                value={product.min_estimate}
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className="col-4">
                             <Form.Label>Max estimate (VNĐ)</Form.Label>
                             <Form.Control
                                 type="text"
+                                name="max_estimate"
                                 placeholder="Max estimate"
                                 className={style.custom_form_control}
-                                value={productArtist}
-                                // onChange={(e) => setProductArtist(e.target.value)}
-                            />
-                        </div>
-                        <div className="col-4">
-                            <Form.Label>Start Bid (VNĐ)</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Start bid"
-                                className={style.custom_form_control}
-                                value={productArtist}
-                                // onChange={(e) => setProductArtist(e.target.value)}
+                                value={product.max_estimate}
+                                onChange={handleInputChange}
                             />
                         </div>
                     </div>
@@ -137,48 +117,20 @@ export default function EditProduct() {
                             <Form.Label>Description</Form.Label>
                             <Form.Control
                                 type="text"
+                                name="description"
                                 placeholder="Description"
                                 className={style.custom_form_control}
-                                value={productDescription}
-                                onChange={(e) => setProductDescription(e.target.value)}
+                                value={product.description}
+                                onChange={handleInputChange}
                             />
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12">
-                            <Form.Label>Image of products (1 -&gt; 5 images)</Form.Label>
-                            <Form.Control
-                                type="file"
-                                placeholder="Image"
-                                className={style.custom_form_control}
-                                name="firstName"
-                                multiple
-                                onChange={handleImageChange}
-                            />
-                        </div>
-
-
                     </div>
                     <div>
-                            <button type="submit" className="btn btn-dark mb-4 col-2" >Change Product</button>
-                            <button type="submit" className="btn btn-light ms-3 mb-4 col-2" onClick={() => { window.location.href = '/seller/my-products'}}>Cancel Change</button>
+                        <button type="submit" className="btn btn-dark mb-4 col-2" >Change Product</button>
+                        <button type="submit" className="btn btn-light ms-3 mb-4 col-2" onClick={() => { window.location.href = '/seller/my-products' }}>Cancel Change</button>
                     </div>
                 </div>
             </form>
-
-            {/* <div>
-                <h3>Danh sách hình ảnh:</h3>
-                {productImages.length > 0 && (
-                    <ul>
-                        {productImages.map((image, index) => (
-                            <li key={index}>
-                                {image.name}
-                                <button type="button" onClick={() => handleRemoveImage(index)}>Xóa</button>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div> */}
         </div >
     );
 }
--- frontend/src/services/product/seller.tsx
+++ frontend/src/services/product/seller.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import { HOST } from '@/services/host';
 import UserDataService from '../model/user';
+import ProductDetail from '@/models/product_detail';
 
 
 let seller_add_product = async (formData: FormData) => {
@@ -20,8 +21,7 @@ let seller_add_product = async (formData: FormData) => {
     }
 }
 
-
-const seller_product_sold_service = async () => {
+let seller_product_sold_service = async () => {
     try {
         let url = `${HOST}/product/seller/sold/user_id=${UserDataService.getUserData()?.user_id}`;
         let response = await axios.get(url);
@@ -32,4 +32,22 @@ const seller_product_sold_service = async () => {
     }
 }
 
-export { seller_add_product, seller_product_sold_service } ;
+let seller_update_product = async (product: ProductDetail, category: string) => {
+    try {
+        let url = `${HOST}/product/seller/update`;
+        const body: any = {};
+        for (const [key, value] of Object.entries(product)) {
+            body[key] = value;
+        }
+        body["category_name"] = category
+        body["user_id"] = UserDataService.getUserData()?.user_id
+
+        const response = await axios.post(url, body)
+        return;
+    } catch (error: any) {
+        console.error('Error fetching data:', error);
+        return error.response.data;
+    }
+}
+
+export { seller_add_product, seller_product_sold_service, seller_update_product } ;
