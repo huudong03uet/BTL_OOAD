01b20d415a4011b11bdde3f80a0f08f2f5ba224b
huudong03uet <huudong03uet@gmail.com>
2024-04-14 05:15:27 +0700

    [Dash] update

--- dashboard/src/components/Modal/ModalSeller.tsx
+++ dashboard/src/components/Modal/ModalSeller.tsx
@@ -36,77 +36,77 @@ function CreateModal(props: IProps) {
         mutate("http://localhost:8000/blogs")
       })
       .catch(function (res) { console.log(res) })
-  
-};
 
-return (
-  <Modal
-    isOpen={showModalCreate}
-    onClose={handleCloseModal}
-    size='lg'
-    style={{ top: '250px', left: '30%', transform: 'translate(-50%, -50%)' }} // Đặt vị trí modal
-  >
-    <ModalContent>
-      <>
-        <ModalHeader className="flex flex-col gap-1">Verification Seller</ModalHeader>
-        <ModalBody style={{ maxHeight: '300px', overflowY: 'auto' }}>
-          <div className="mb-0">
-            <strong>Seller Information:</strong>
-          </div>
-          <div className="mb-0 ml-3">
-            <p><strong>Seller Name:</strong> {name}</p>
-          </div>
-          <div className="mb-0 flex-row" style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <p className="ml-3"><strong>Email:</strong> {email}</p>
-            <p className="mr-20"><strong>Phone Number:</strong> {phoneNumber}</p>
-          </div>
-          <div className="mb-1 ml-3">
-            <p><strong>Description:</strong> {description}</p>
-          </div>
-          <div className="mb-0">
-            <strong>Card Information:</strong>
-          </div>
-          <div className="mb-0 ml-3">
-            <p><strong>Card Number:</strong> {card_number}</p>
-          </div>
-          <div className="mb-0 flex-row" style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <p className='ml-3'><strong>Expiry:</strong> {expiry}</p>
-            <p className='mr-40'><strong>CVN:</strong> {cvn}</p>
-          </div>
-          <div className="mb-1 ml-3">
-            <p><strong>Name on Card:</strong> {nameOnCard}</p>
-          </div>
+  };
 
-          <div className="mb-0">
-            <strong>Address Information:</strong>
-          </div>
-          <div className="mb-0 ml-2">
-            <p><strong>Country:</strong> {country}</p>
-          </div>
-          <div className="mb-0 ml-3">
-            <p><strong>Address:</strong> {address}</p>
-          </div>
-          <div className="mb-0 flex-row" style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <p className='ml-3'><strong>City:</strong> {city}</p>
-            <p className='mr-40'><strong>State:</strong> {state}</p>
-          </div>
-          <div className="mb-0 ml-3">
-            <p><strong>Postal Code:</strong> {postalCode}</p>
-          </div>
-        </ModalBody>
-        <ModalFooter style={{ justifyContent: 'space-between' }}>
-          <Button color="danger" onPress={handleCloseModal}>
-            Reject
-          </Button>
-          <Button color="primary" onPress={handleCloseModal}>
-            Accept
-          </Button>
-        </ModalFooter>
-      </>
-    </ModalContent>
-  </Modal>
+  return (
+    <Modal
+      isOpen={showModalCreate}
+      onClose={handleCloseModal}
+      size='3xl'
+      style={{ top: '250px', left: '30%', transform: 'translate(-50%, -50%)' }} // Đặt vị trí modal
+    >
+      <ModalContent>
+        <>
+          <ModalHeader className="flex flex-col gap-1">Verification Seller</ModalHeader>
+          <ModalBody style={{ maxHeight: '300px', overflowY: 'auto' }}>
+            <div className="mb-0">
+              <strong>Seller Information:</strong>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Seller Name:</strong> {name}</p>
+            </div>
+            <div className="mb-0 flex-row" style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className="ml-3"><strong>Email:</strong> {email}</p>
+              <p className="mr-20"><strong>Phone Number:</strong> {phoneNumber}</p>
+            </div>
+            <div className="mb-1 ml-3">
+              <p><strong>Description:</strong> {description}</p>
+            </div>
+            <div className="mb-0">
+              <strong>Card Information:</strong>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Card Number:</strong> {card_number}</p>
+            </div>
+            <div className="mb-0 flex-row" style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className='ml-3'><strong>Expiry:</strong> {expiry}</p>
+              <p className='mr-40'><strong>CVN:</strong> {cvn}</p>
+            </div>
+            <div className="mb-1 ml-3">
+              <p><strong>Name on Card:</strong> {nameOnCard}</p>
+            </div>
 
-);
+            <div className="mb-0">
+              <strong>Address Information:</strong>
+            </div>
+            <div className="mb-0 ml-2">
+              <p><strong>Country:</strong> {country}</p>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Address:</strong> {address}</p>
+            </div>
+            <div className="mb-0 flex-row" style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className='ml-3'><strong>City:</strong> {city}</p>
+              <p className='mr-40'><strong>State:</strong> {state}</p>
+            </div>
+            <div className="mb-0 ml-3">
+              <p><strong>Postal Code:</strong> {postalCode}</p>
+            </div>
+          </ModalBody>
+          <ModalFooter style={{ justifyContent: 'space-between' }}>
+            <Button color="danger" onPress={handleCloseModal}>
+              Reject
+            </Button>
+            <Button color="primary" onPress={handleCloseModal}>
+              Accept
+            </Button>
+          </ModalFooter>
+        </>
+      </ModalContent>
+    </Modal>
+
+  );
 }
 
 export default CreateModal;
