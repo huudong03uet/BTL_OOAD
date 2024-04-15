9eb4b6425624ccd449601782b98729eef98d0bfc
Nguyen Hien <hiennguyenhien1105@gmail.com>
2024-04-15 19:11:32 +0700

    add product detail

--- frontend/src/app/seller/component/product-table.tsx
+++ frontend/src/app/seller/component/product-table.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
@@ -14,6 +15,7 @@ import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import ItemSummary from '@/models/product_summary';
+import { user_get_detail_product } from "@/services/product/user";
 
 export enum TableActivity {
     VIEW_MY_PRODUCT = 0,
@@ -35,11 +37,22 @@ const MyProductTable: React.FC<MyProductTableProps> = ({
     setAuctionStates = () => { },
 }) => {
     const [open, setOpen] = useState<number | null>(null);
-
-    const handleRowClick = (index: number) => {
-        setOpen((prevOpen) => (prevOpen === index ? null : index));
+    const [selectedRow, setSelectedRow] = useState<number | null>(null);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const handleRowClick = async (id) => {
+        try {
+            const productId = Number(id);
+            const productData = await user_get_detail_product(productId);
+            console.log('Product detail:', productData);
+            await setSelectedProduct(productData);
+            console.log(selectedProduct);
+        } catch (error) {
+            console.error('Error fetching product detail:', error);
+        }
     };
-
+    const handleCloseModal = () => {
+        setSelectedProduct(null);
+    };
     const handleAction = (index: number) => {
         if (activity === TableActivity.VIEW_MY_PRODUCT) {
             window.location.href = `/seller/edit-product?id=${data[index].id}`;
@@ -51,50 +64,51 @@ const MyProductTable: React.FC<MyProductTableProps> = ({
     };
 
     return (
-        <TableContainer component={Paper}>
-            <Table aria-label="collapsible table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="center" style={{ width: '5%' }}>ID</TableCell>
-                        <TableCell align="center" style={{ width: 'auto' }}>Name</TableCell>
-                        <TableCell align="center" style={{ width: '10%' }}>Min estimate</TableCell>
-                        <TableCell align="center" style={{ width: '10%' }}>Max estimate</TableCell>
-                        <TableCell align="center" style={{ width: '10%' }}>Max Bid</TableCell>
-                        <TableCell align="center" style={{ width: '10%' }}>Status</TableCell>
-                        {(activity === TableActivity.VIEW_MY_PRODUCT || activity === TableActivity.ADD_TO_AUCTION || activity === TableActivity.VIEW_IN_AUCTION) && (
-                            <TableCell align="center" style={{ width: '10%' }}>{activity === TableActivity.VIEW_MY_PRODUCT ? 'Edit' : 'Action'}</TableCell>
-                        )}
-                        <TableCell align="center" style={{ width: '5%' }}>Detail</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.map((row, index) => (
-                        <React.Fragment key={row.id}>
-                            <TableRow>
-                                <TableCell align="center">{index + 1}</TableCell>
-                                <TableCell>{row.title}</TableCell>
-                                <TableCell align="center">{row.estimate_min}</TableCell>
-                                <TableCell align="center">{row.estimate_max}</TableCell>
-                                <TableCell align="center">{row.max_bid}</TableCell>
-                                <TableCell align="center">{row.status}</TableCell>
-                                {(activity === TableActivity.VIEW_MY_PRODUCT || activity === TableActivity.ADD_TO_AUCTION || activity === TableActivity.VIEW_IN_AUCTION) && (
+        <>
+            <TableContainer component={Paper}>
+                <Table aria-label="collapsible table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center" style={{ width: '5%' }}>ID</TableCell>
+                            <TableCell align="center" style={{ width: 'auto' }}>Name</TableCell>
+                            <TableCell align="center" style={{ width: '10%' }}>Min estimate</TableCell>
+                            <TableCell align="center" style={{ width: '10%' }}>Max estimate</TableCell>
+                            <TableCell align="center" style={{ width: '10%' }}>Max Bid</TableCell>
+                            <TableCell align="center" style={{ width: '10%' }}>Status</TableCell>
+                            {(activity === TableActivity.VIEW_MY_PRODUCT || activity === TableActivity.ADD_TO_AUCTION || activity === TableActivity.VIEW_IN_AUCTION) && (
+                                <TableCell align="center" style={{ width: '10%' }}>{activity === TableActivity.VIEW_MY_PRODUCT ? 'Edit' : 'Action'}</TableCell>
+                            )}
+                            <TableCell align="center" style={{ width: '5%' }}>Detail</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map((row, index) => (
+                            <React.Fragment key={row.id}>
+                                <TableRow>
+                                    <TableCell align="center">{index + 1}</TableCell>
+                                    <TableCell>{row.title}</TableCell>
+                                    <TableCell align="center">{row.estimate_min}</TableCell>
+                                    <TableCell align="center">{row.estimate_max}</TableCell>
+                                    <TableCell align="center">{row.max_bid}</TableCell>
+                                    <TableCell align="center">{row.status}</TableCell>
+                                    {(activity === TableActivity.VIEW_MY_PRODUCT || activity === TableActivity.ADD_TO_AUCTION || activity === TableActivity.VIEW_IN_AUCTION) && (
+                                        <TableCell align="center">
+                                            <button className="btn btn-primary w-100" onClick={() => handleAction(index)}>
+                                                {activity === TableActivity.VIEW_MY_PRODUCT ? 'Edit' : auctionStates[index] ? 'Remove' : 'Add'}
+                                            </button>
+                                        </TableCell>
+                                    )}
                                     <TableCell align="center">
-                                        <button className="btn btn-primary w-100" onClick={() => handleAction(index)}>
-                                            {activity === TableActivity.VIEW_MY_PRODUCT ? 'Edit' : auctionStates[index] ? 'Remove' : 'Add'}
-                                        </button>
+                                        <IconButton
+                                            aria-label="expand row"
+                                            size="small"
+                                            onClick={() => handleRowClick(row.id)}
+                                        >
+                                            {open === index ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                                        </IconButton>
                                     </TableCell>
-                                )}
-                                <TableCell align="center">
-                                    <IconButton
-                                        aria-label="expand row"
-                                        size="small"
-                                        onClick={() => handleRowClick(index)}
-                                    >
-                                        {open === index ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                                    </IconButton>
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
+                                </TableRow>
+                                {/* <TableRow>
                                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
                                     <Collapse in={open === index} timeout="auto" unmountOnExit>
                                         <Box sx={{ margin: 1 }}>
@@ -121,12 +135,40 @@ const MyProductTable: React.FC<MyProductTableProps> = ({
                                         </Box>
                                     </Collapse>
                                 </TableCell>
-                            </TableRow>
-                        </React.Fragment>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                            </TableRow> */}
+                            </React.Fragment>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Modal
+                open={selectedProduct !== null}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-title"
+                aria-describedby="modal-description"
+                size="lg"
+            >
+
+                <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
+                    <Typography id="modal-title" variant="h6" component="h2">
+                        <h1 className='text-center'>Product Details</h1>
+                    </Typography>
+                    <Typography id="modal-description" sx={{ mt: 2 }}>
+                        {selectedProduct && (
+                            <>
+                                <span style={{ fontWeight: "bold" }}>Name:</span> {selectedProduct.title} <br />
+                                <span style={{ fontWeight: "bold" }}>Description:</span> {selectedProduct.description} <br />
+                                <span style={{ fontWeight: "bold" }}>Min Estimate:</span> {selectedProduct.min_estimate} <br />
+                                <span style={{ fontWeight: "bold" }}>Max Estimate:</span> {selectedProduct.max_estimate} <br />
+                                <span style={{ fontWeight: "bold" }}>Action Id:</span> {selectedProduct.auction_id} <br />
+                                <span style={{ fontWeight: "bold" }}>Status:</span> {selectedProduct.status} <br />
+                                <span style={{ fontWeight: "bold" }}>Created At:</span> {selectedProduct.createdAt} <br />
+                            </>
+                        )}
+                    </Typography>
+                </Box>
+            </Modal>
+        </>
     );
 };
 
