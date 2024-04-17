52d07af9b2b55e58d9f6c2353d033f4a9312cc8b
huudong03uet <huudong03uet@gmail.com>
2024-04-18 03:07:46 +0700

    [FE] fix

--- frontend/src/components/shared/promotedAuctions.tsx
+++ frontend/src/components/shared/promotedAuctions.tsx
@@ -1,27 +1,27 @@
-import AuctionSummary from '@/models/auction_summary';
+import Auction from '@/models/auction';
 import React from 'react'
 import dateFormat, { masks } from "dateformat";
 
-function PromotedAuctions({ obj }: { obj: AuctionSummary }) {
+function PromotedAuctions({ obj }: { obj: Auction }) {
     return (
         <div style={{ border: "1px solid #bac4c9" }}>
             <div className="container">
                 <div className="row">
                     <div className="col-6 p-0 d-flex justify-content-center">
-                        <img src={obj.image_path} style={{ width: "auto", height: "auto", maxWidth: "100%", maxHeight:"200px" }} />
+                        <img src={obj.products?.[0].images?.[0].url} style={{ width: "auto", height: "auto", maxWidth: "100%", maxHeight:"200px" }} />
                     </div>
                     <div className="col-6 ps-0">
 
                         <div className='my-1 fw-bold'
                             style={{ display: "-webkit-box", WebkitBoxOrient: "vertical", WebkitLineClamp: 2, overflow: "hidden" }}
-                        >                 {obj.title}
+                        >                 {obj.name}
                         </div>
                         <div className="text-truncate">
-                            by {obj.seller_name}
+                            by {obj.name}
                         </div>
                         <div className="my-3">
                             {/* {obj.time} */}
-                            {dateFormat(obj.time, " mmm dd, yyyy - hh:MM TT")}
+                            {dateFormat(obj.time_auction, " mmm dd, yyyy - hh:MM TT")}
                         </div>
                         <button type="button" className="btn btn-danger w-100"
                         onClick={() =>  {
